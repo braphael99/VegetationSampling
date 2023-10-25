62afbdb33020a93739073452a9717e854fdbc078
BLAKE RAPHAEL <braphael99@gmail.com>
2023-10-24 21:18:48 -0600

    Added template HTML pages and Flask app routes for corresponding pages

--- python/app.py
+++ python/app.py
@@ -1,13 +1,16 @@
+import requests
 import sqlite3
 from sqlite3 import Error
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import math
 from io import BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import jinja2
 plt.style.use('ggplot')
+matplotlib.use('agg')
 
 from flask import Flask
 from flask import abort
@@ -15,38 +18,28 @@ from flask import request
 from flask import redirect
 from flask import render_template
 from flask import send_file
+from flask import jsonify
 
 app = Flask(__name__)
 
-#import requests
-
-#retrieve all national parks and pipe them into our local DB
-#def fetch_parks_from_api():
-    #api_url = 'https://developer.nps.gov/api/v1/parks?api_key=pP814qNL44jE9aIH9tXlbgNS3Ddn428wmokwPT4a'
-    #response = requests.get(api_url)
-    #if response.status_code == 200:
-        #return response.json()
-    #else:
-        #return None
-
-@app.route("/age")
-def getAges():
-    ages = []
+@app.route("/CBH")
+def getCBH():
+    circumferences = []
     conn = None
     try:
-        conn = sqlite3.connect("../dbs/birthweight.db")
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
         conn.row_factory = sqlite3.Row
         sql = """ 
-            SELECT birthwt.ID, birthwt.age
-            FROM birthwt
-            ORDER BY birthwt.ID
+            SELECT vegetation_sampling.id, vegetation_sampling.CBH
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
         """
         cursor = conn.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()  
         for row in rows:
-            age = {"id": row["id"], "age": row["age"]}
-            ages.append(age)
+            circumference = {"id": row["id"], "circumference": row["CBH"]}
+            circumferences.append(circumference)
 
     except Error as e:
         print(f"Error opening the database {e}")
@@ -55,26 +48,26 @@ def getAges():
         if conn:
             conn.close()
 
-    return {"Ages": ages} 
+    return {"Circumferences": circumferences} 
 
-@app.route("/age/average")
-def getAvgAge():
-    ages = []
+@app.route("/height")
+def getHeight():
+    heights = []
     conn = None
     try:
-        conn = sqlite3.connect("../dbs/birthweight.db")
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
         conn.row_factory = sqlite3.Row
         sql = """ 
-            SELECT birthwt.age
-            FROM birthwt
-            ORDER BY birthwt.ID
+            SELECT vegetation_sampling.id, vegetation_sampling.height
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
         """
         cursor = conn.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()  
         for row in rows:
-            age = row["age"]
-            ages.append(age)
+            height = {"id": row["id"], "height": row["height"]}
+            heights.append(height)
 
     except Error as e:
         print(f"Error opening the database {e}")
@@ -82,31 +75,144 @@ def getAvgAge():
     finally:
         if conn:
             conn.close()
-    average = sum(ages)/len(ages)
-    return {"Average Age": average} 
 
-@app.route("/ageweight/")
+    return {"Heights": heights} 
+
+@app.route("/DBH")
+def getDBH():
+    diameters = []
+    conn = None
+    try:
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
+        conn.row_factory = sqlite3.Row
+        sql = """ 
+            SELECT vegetation_sampling.id, vegetation_sampling.DBH
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
+        """
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()  
+        for row in rows:
+            diameter = {"id": row["id"], "diameter": row["DBH"]}
+            diameters.append(diameter)
+
+    except Error as e:
+        print(f"Error opening the database {e}")
+        abort(500)
+    finally:
+        if conn:
+            conn.close()
+
+    return {"Diameters": diameters} 
+
+@app.route("/BA")
+def getBA():
+    basiAreas = []
+    conn = None
+    try:
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
+        conn.row_factory = sqlite3.Row
+        sql = """ 
+            SELECT vegetation_sampling.id, vegetation_sampling.BA
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
+        """
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()  
+        for row in rows:
+            basiArea = {"id": row["id"], "basiArea": row["BA"]}
+            basiAreas.append(basiArea)
+
+    except Error as e:
+        print(f"Error opening the database {e}")
+        abort(500)
+    finally:
+        if conn:
+            conn.close()
+
+    return {"Basimetric Areas": basiAreas} 
+
+@app.route("/dead")
+def getDead():
+    deaths = []
+    conn = None
+    try:
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
+        conn.row_factory = sqlite3.Row
+        sql = """ 
+            SELECT vegetation_sampling.id, vegetation_sampling.dead
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
+        """
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()  
+        for row in rows:
+            dead = {"id": row["id"], "dead": row["dead"]}
+            deaths.append(dead)
+
+    except Error as e:
+        print(f"Error opening the database {e}")
+        abort(500)
+    finally:
+        if conn:
+            conn.close()
+
+    return {"Dead": deaths} 
+
+
+@app.route("/DBHClass")
+def getDBHClass():
+    classes = []
+    conn = None
+    try:
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
+        conn.row_factory = sqlite3.Row
+        sql = """ 
+            SELECT vegetation_sampling.id, vegetation_sampling.DBHclass
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
+        """
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()  
+        for row in rows:
+            dClass = {"id": row["id"], "DBH Class": row["DBHclass"]}
+            classes.append(dClass)
+
+    except Error as e:
+        print(f"Error opening the database {e}")
+        abort(500)
+    finally:
+        if conn:
+            conn.close()
+
+    return {"Classes": classes} 
+
+@app.route("/circumHeight/")
 def ageweightViz():
-    ages =[]
-    weights = []
+    circumferences =[]
+    heights = []
     conn = None
 
     try:
-        conn = sqlite3.connect("../dbs/birthweight.db")
+        conn = sqlite3.connect("../dbs/vegetationSampling.db")
         conn.row_factory = sqlite3.Row
         sql = """ 
-            SELECT birthwt.age, birthwt.lwt
-            FROM birthwt
-            ORDER BY birthwt.ID
+            SELECT vegetation_sampling.CBH, vegetation_sampling.height
+            FROM vegetation_sampling
+            ORDER BY vegetation_sampling.id
         """
         cursor = conn.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()  
         for row in rows:
-            age = row["age"]
-            weight = row["lwt"]
-            ages.append(age)
-            weights.append(weight)
+            circumference = row["CBH"]
+            height = row["height"]
+            circumferences.append(circumference)
+            heights.append(height)
 
     except Error as e:
         print(f"Error opening the database {e}")
@@ -115,18 +221,35 @@ def ageweightViz():
         if conn:
             conn.close()
     fig, ax = plt.subplots()
-    plt.scatter(ages, weights)
-    plt.xlabel("Ages")
-    plt.ylabel("Weights")
-    plt.title("Mother's Age vs Last Menstural Weight") 
+    plt.scatter(circumferences, heights)
+    plt.xlabel("Circumferences (in CM)")
+    plt.ylabel("Heights (in Meters)")
+    plt.title("Circumferences vs. Heights") 
     canvas = FigureCanvas(fig)
     img = BytesIO()
     fig.savefig(img)
     img.seek(0)
     return send_file(img, mimetype = "image/png")
 
-@app.route("/home")
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/circumferencePage")
+def circumferencePage():
+    return render_template("circumferencePage.html")
+
+@app.route("/diameterPage")
+def diameterPage():
+    return render_template("diameterPage.html")
+
+@app.route("/basiPage")
+def basiPage():
+    return render_template("basiPage.html")
+
+@app.route("/predictiveStats")
+def predictiveStats():
+    return render_template("predictiveStats.html")
+
 if __name__ == '__main__':
    app.run()
--- python/venv/Scripts/activate.bat
+++ python/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\braph\Desktop\Portfolio\NPSDataService\python\venv
+set VIRTUAL_ENV=C:\Users\braph\Desktop\Portfolio\VegetationSampling\python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
