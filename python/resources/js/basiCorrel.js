669e603e18ac367816d9cf1a70c31a02f6af54f5
BLAKE RAPHAEL <braphael99@gmail.com>
2023-10-28 15:50:24 -0600

    Added hypothesisTesting.js and linearRegression.js, updated basiCorrel.js and diamCorrel.js

---
+++ python/resources/js/basiCorrel.js
@@ -0,0 +1,29 @@
+fetch('/basiCorrel')
+.then(response => response.json())
+.then(data => {
+    const correlation = data.correlation;
+// Manipulate the HTML to display the JSON data
+    document.getElementById('basiHolder').innerText = `The Correlation Coefficient is: ${correlation}`
+    if (correlation >= -1.0 && correlation < -0.6){
+        document.getElementById("correlationInterp").innerText = "The correlation is a very strongly negative correlation.";
+    }
+    else if (correlation >= -0.6 && correlation < -0.3){
+        document.getElementById("correlationInterp").innerText = "The correlation is a negative correlation.";
+    }
+    else if (correlation >= -0.3 && correlation < 0){
+        document.getElementById("correlationInterp").innerText = "The correlation is a very weak negative correlation.";
+    }
+    else if (correlation >= 0 && correlation < 0.3){
+        document.getElementById("correlationInterp").innerText = "The correlation is a very weak positive correlation.";
+    }
+    else if (correlation >= 0.3 && correlation < 0.6){
+        document.getElementById("correlationInterp").innerText = "The correlation is a positive correlation.";
+    }
+    else{
+        document.getElementById("correlationInterp").innerText = "The correlation is a very strong postitive correlation.";
+    }
+
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
